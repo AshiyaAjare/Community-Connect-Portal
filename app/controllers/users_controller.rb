0c2031d9ff1ced2ca0b411419f28dc8280cd335b
Ashiya Ajare <ashiya.ajare@joshsoftware.com>
2025-01-27 16:27:14 +0530

    Add controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,39 @@ class UsersController < ApplicationController
         @users = User.find(params[:id])
     end
 
+    def new
+        @user = User.new
+      end
     
+      def create
+        params[:user][:role] = params[:user][:role].to_i
+    
+        @user = User.new(user_params)
+        #Due to strong parameters
+        
+        if @user.save
+          redirect_to user_path(@user) , notice: "User created"
+        else
+          render :new
+        end
+    end
+
+    def update
+        if @user.update(user_params)
+          redirect_to users_path, notice: "User updated successfully."
+        else
+          render :edit
+        end
+    end
+    
+      # Set user based on ID from params
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        #byebug
+        params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :profile_image_url, :role)
+    end
+
 end
